cf8c9054628490cfe62957e6d3dc9c4751e85fd8
guesstheage-pro <guesstheage.info@gmail.com>
2026-09-15 17:44:08 +0200

    Delete nfc-links-main/nfc-links-main directory

--- nfc-links-main/nfc-links-main/src/app/src/app/src/lib/utils.ts
+++
@@ -1,8 +0,0 @@
-export function generateShortCode(length = 5) {
-  const chars = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
-  let result = '';
-  for (let i = 0; i < length; i++) {
-    result += chars.charAt(Math.floor(Math.random() * chars.length));
-  }
-  return result;
-}
